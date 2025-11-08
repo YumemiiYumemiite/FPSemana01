214fa52aa53f970c070659383b2fe6fcb19d73ea
Emily Batista“git config --global user.name Emily <emilybatista.programmer@hotmail.com>
2025-11-08 00:10:04 +0000

    First Commit

--- character.py
+++ character.py
@@ -12,19 +12,19 @@ def criar_personagem():
 
 personagens = [criar_personagem() for _ in range(3)]
 
+print()
+print([
+    [personagens[0].nome, (personagens[0].ataque, personagens[0].defesa)],
+    [personagens[1].nome, (personagens[0].ataque, personagens[1].defesa)],
+    [personagens[2].nome, (personagens[2].ataque, personagens[2].defesa)]
+])
+
 maior_ataque = max(p.ataque for p in personagens)
 maior_defesa = max(p.defesa for p in personagens)
 
 melhor_ataque = next(p for p in personagens if p.ataque == maior_ataque)
 melhor_defesa = next(p for p in personagens if p.defesa == maior_defesa)
 
-print()
+
 print(f"Ataque {melhor_ataque.nome} {melhor_ataque.ataque}")
 print(f"Defesa {melhor_defesa.nome} {melhor_defesa.defesa}")
-
-
-print()
-for p in personagens:
-    print(p.nome)
-    print(p.ataque)
-    print(p.defesa)
